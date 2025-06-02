e8be7e971b26054122b0a82d1e0e79f9ef0a34eb
sardor-an <sardorxakimov200@gmail.com>
2025-06-02 16:17:23 +0500

    it was just a warm up!

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l63*md!q55%qi^t*wh&8z7^$_^!4@!5p!7utmuun$t&r_xs#+x'
+SECRET_KEY = 'django-insecure-=lqqkqi#qh!c0r=bhl)%++j(8r&wbfz)uwq5(^pj+2j=yg8ov6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
